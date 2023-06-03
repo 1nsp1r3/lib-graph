f986f7dde1ed6b66287c6554220fcd01c230d642
1nsp1r3 <57573816+1nsp1r3@users.noreply.github.com>
2023-06-03 12:36:05 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,54 @@
+const Chart = require("../lib/chartjs-4.3.0.min.js")
+
+let chart = null
+
+/**
+ * Data = [
+ *   { label: 2010, value: 10 },
+ *   { label: 2011, value: 20 },
+ *   { label: 2012, value: 15 },
+ *  ]
+ */
+const init = function(CanvasId, Legend, Data){
+  const canvas = document.getElementById(CanvasId)
+
+  let labels = []
+  let values = []
+
+  if (Data){
+    labels = Data.map(it => it.label)
+    values = Data.map(it => it.value)
+  }
+
+  const options = {
+    type: 'line',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: Legend,
+        data: values,
+      }],
+    },
+  }
+  chart = new Chart(canvas, options)
+}
+
+/**
+ * {
+ *   label: 2010,
+ *   value: 8,
+ * }
+ */
+const addData = function(Data){
+  chart.data.labels.push(Data.label);
+  chart.data.datasets.forEach((dataset) => dataset.data.push(Data.value))
+  chart.update()
+}
+
+/**
+ * CommonJS format for NodeJS and Espruino
+ */
+module.exports = {
+  init,
+  addData,
+}
