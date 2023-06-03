71c4959a2a6889a6a17df29f516921058a81470f
1nsp1r3 <abel.andouard@gmail.com>
2023-06-03 14:47:50 +0200

    Stop using chartjs from npm

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # graph
+
+## chart.js
+`lib/chart-4.3.0.min.js` come from https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.3.0/chart.umd.min.js
--- src/main.js
+++ src/main.js
@@ -1,54 +1,56 @@
-const Chart = require("chartjs")
+const Chart = require("../lib/chart-4.3.0.min.js")
 
 let chart = null
 
-/**
- * Data = [
- *   { label: 2010, value: 10 },
- *   { label: 2011, value: 20 },
- *   { label: 2012, value: 15 },
- *  ]
- */
-const init = function(CanvasId, Legend, Data){
-  const canvas = document.getElementById(CanvasId)
+class Graph {
+  /**
+   * Data = [
+   *   { label: 2010, value: 10 },
+   *   { label: 2011, value: 20 },
+   *   { label: 2012, value: 15 },
+   *  ]
+   */
+  constructor(CanvasId, Legend, Data){
+    this.canvas = document.getElementById(CanvasId)
 
-  let labels = []
-  let values = []
+    let labels = []
+    let values = []
 
-  if (Data){
-    labels = Data.map(it => it.label)
-    values = Data.map(it => it.value)
+    if (Data){
+      labels = Data.map(it => it.label)
+      values = Data.map(it => it.value)
+    }
+
+    this.options = {
+      type: 'line',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: Legend,
+          data: values,
+        }],
+      },
+    }
   }
 
-  const options = {
-    type: 'line',
-    data: {
-      labels: labels,
-      datasets: [{
-        label: Legend,
-        data: values,
-      }],
-    },
+  display(){
+    this.chart = new Chart(this.canvas, this.options)
   }
-  chart = new Chart(canvas, options)
-}
 
-/**
- * {
- *   label: 2010,
- *   value: 8,
- * }
- */
-const addData = function(Data){
-  chart.data.labels.push(Data.label);
-  chart.data.datasets.forEach((dataset) => dataset.data.push(Data.value))
-  chart.update()
+  /**
+   * {
+   *   label: 2010,
+   *   value: 8,
+   * }
+   */
+  addData(Data){
+    this.chart.data.labels.push(Data.label);
+    this.chart.data.datasets.forEach((dataset) => dataset.data.push(Data.value))
+    this.chart.update()
+  }
 }
 
 /**
  * CommonJS format for NodeJS and Espruino
  */
-module.exports = {
-  init,
-  addData,
-}
+module.exports = Graph
